bc7e29c7b5203d4af32177a7ed5746637b08bad6
Chiefogbe <watchmanjesse5002@gmail.com>
2023-12-09 06:27:39 +0100

    install react tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h4 className='text-red-500 text-center font-bold font-serif my-4'>Welcome To Real Estate Mern App.</h4>
+    </div>
+  )
+}
+
+export default App
